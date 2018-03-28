374c28ed5344c3f7dcd7e2a2aa4a899465d99768
guotianbao <gmu1592618@gmail.com>
2018-03-28 18:15:54 +0800

    反向打印链表

--- Algorithm/面试题/反向打印链表.py
+++ Algorithm/面试题/反向打印链表.py
@@ -2,27 +2,29 @@
 输入一个链表，从尾到头打印链表每个节点的值。
 '''
 class ListNode:
-	def __init__(self,x=None):
-		self.val = x
-		self.next = None
-		
+    def __init__(self, x=None):
+        self.val = x
+        self.next = None
+
 class Solution:
-	def printListFormTailToHead(self,listNode):
-		if listNode.val == None
-			return
-		l = []
-		head = listNode
-		while head:
-			l.insert(0,head.val)
-			head = head	.next
-		return l
+    def printListFormTailToHead(self, listNode):
+        if listNode.val == None:
+            return
+        l = []
+        head = listNode
+        while head:
+            l.insert(0, head.val)
+            head = head.next
+        return l
 
 node1 = ListNode(10)
 node2 = ListNode(11)
 node3 = ListNode(13)
 node1.next = node2
 node2.next = node3
+
 singleNode = ListNode(12)
+
 test = ListNode()
 
 S = Solution()
